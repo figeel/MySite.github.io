f0f92b80c1d37ebbc01d0479abbcc8d774dfedcd
Ilya Lysenko <165323467+figeel@users.noreply.github.com>
2025-03-05 20:42:10 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-button');
+const navBtnImg = document.querySelector('#nav-button-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./img/icons/nav-close.svg"
+    } else {
+        navBtnImg.src = "./img/icons/nav-open.svg"
+    }
+}
+
+AOS.init({
+    disable: 'mobile',
+    once:true
+});
